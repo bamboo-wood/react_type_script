e9449c179bc420a00ad826f655229126f78c0fdb
bamboo-wood <cantate.1009.0417@gmail.com>
2023-04-04 22:28:19 +0900

    add type list

---
+++ src/typeList.ts
@@ -0,0 +1,40 @@
+export {};
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+// TypeScriptの基本の型
+
+// boolean
+let bool: boolean = true;
+
+// number
+let bucket: number = 10;
+
+// string
+let name: string = "Ham";
+
+// Array
+let array: number[] = [0, 1, 2];
+let array2: Array<number> = [0, 1, 2];
+
+// tuple
+let tuple: [number, string] = [0, "1"];
+
+// any
+let any1: any = false;
+
+// void
+const funcA = (): void => {
+  const test = "test";
+};
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+// object
+let object1: object = {};
+
+// 関数の型定義
